667807cdb2241b513d687715d5a5ac2bfbb68fb2
Glenn Drescher <GlennD@me.com>
2020-03-06 00:00:11 +0100

    Initial Commit

---
+++ White Petition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  White Petition
+//
+//  Created by Glenn Drescher on 06/03/2020.
+//  Copyright © 2020 GDev.run. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
